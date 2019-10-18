fde86bb513e816381a172e4de6b6afb542e9c9f1
Gavin White <horace5413@gmail.com>
2019-10-17 17:45:21 -0700

    Backbone structures for rendering.

---
+++ confyre/confyre/Math/Trig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace confyre.Math {
+    internal static class Trig {
+        public static int Distance(Render.Position.Position pos1, Render.Position.Position pos2) =>
+            (int)System.Math.Sqrt((pos1.X - pos2.X) * (pos1.X - pos2.X) + (pos1.Y - pos2.Y) * (pos1.Y - pos2.Y));
+    }
+}
---
+++ confyre/confyre/Render/Position/Pair.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace confyre.Render.Position {
+    internal class Pair<T> {
+        internal T X { get => values[0]; set => values[0] = value; }
+        internal T Y { get => values[1]; set => values[1] = value; }
+
+        T[] values = new T[2];
+
+        internal Pair() { }
+        internal Pair(T x, T y) {
+            values[0] = x;
+            values[1] = y;
+        }
+        ~Pair() { }
+    }
+}
